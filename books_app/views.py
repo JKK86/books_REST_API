7062a4150ba0afb94a665c6e3e7e352da5b23bc2
JKK86 <j.kurowski86@wp.pl>
2021-10-01 21:02:32 +0200

    create generic views for books list and book details

--- books_app/views.py
+++ books_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from books_app.models import Book
+from books_app.serializers import BookSerializer
+
+
+class BooksListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
